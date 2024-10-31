e7d182ab1a94c2051c7616c9115a4f6aaeadc004
Kilian <96377170+kilianmendez@users.noreply.github.com>
2024-10-31 16:26:18 +0100

    Primer commit del back a develop

---
+++ SolessBackEndFix/SolessBackEndFix/DTO/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SolessBackend.DTO
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+        public string? Address { get; set; }
+    }
+}
---
+++ SolessBackEndFix/SolessBackEndFix/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using SolessBackend.Data;
+using SolessBackend.Interfaces;
+using SolessBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolessBackend.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataBaseContext _context;
+        public UserRepository(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<User>> GetUsersAsync()
+        {
+            return await _context.Users.OrderBy(u => u.Id).ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(long id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
