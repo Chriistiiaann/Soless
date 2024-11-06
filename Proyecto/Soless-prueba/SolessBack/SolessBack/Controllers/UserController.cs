601fc4192b6e1c4e0e9d7386a253af613496178a
Juan Jose Tejada <jtejgut1802@g.educaand.es>
2024-11-06 15:45:04 +0100

    BBDD completa

---
+++ Proyecto/Soless-prueba/SolessBack/SolessBack/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaSolessChristian.Models.Database;
+using PruebaSolessChristian.Models.Database.Repositories;
+using SolessBack.Models.Dtos;
+
+namespace PruebaSolessChristian.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly UserRepository _userRepository;
+
+    public UserController(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    [HttpGet("ObtenerUsers")]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        var users = await _userRepository.GetAllAsync();
+        return Ok(new UserListDto(users));
+    }
+
+    [HttpGet("ObtenerUserById")]
+    public async Task<ActionResult<User>> GetUserById(int id)
+    {
+        var user = await _userRepository.GetByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userInfo = new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            Name = user.Name,
+            Role = user.Role ?? "none",
+            Address = user.Address
+        };
+
+        return Ok(userInfo);
+    }
+
+    [HttpPost("CrearUsuario")]
+    public async Task<ActionResult<User>> CreateUser([FromBody] CrearUserDto model)
+    {
+
+        var user = new User
+        {
+            Name = model.Name,
+            Address = model.Address,
+            Email = model.Email,
+            Password = model.Password,
+            Role = model.Role ?? "none"
+        };
+
+        var createdUser = await _userRepository.InsertUserAsync(user);
+
+        return CreatedAtAction(nameof(GetUserById), new { id = createdUser.Id }, createdUser);
+    }
+
+    [HttpDelete("BorrarUsuario")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _userRepository.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        await _userRepository.DeleteAsync(user);
+
+        return NoContent();
+    }
+}
---
+++ Proyecto/Soless-prueba/SolessBackEndFix/SolessBackEndFix/Data/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolessBackend.Models;
+using SolessBackEndFix.Models;
+
+namespace SolessBackend.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        private const string DATABASE_PATH = "ecommerce.db";
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            optionsBuilder.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+        }
+    }
+}
