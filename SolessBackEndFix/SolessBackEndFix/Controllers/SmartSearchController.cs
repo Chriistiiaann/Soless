ebc75104fb61c8c7750d3e0a52c3db4f9b307ba6
Christian Rodríguez Lara <christianrodriguezlara6@gmail.com>
2024-11-08 20:25:26 +0100

    Cambiado FromQuery a Frombody en SmartSearchController

--- SolessBackEndFix/SolessBackEndFix/Controllers/SmartSearchController.cs
+++ SolessBackEndFix/SolessBackEndFix/Controllers/SmartSearchController.cs
@@ -1,7 +1,8 @@
 ﻿using Examples.WebApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using SolessBackEndFix.DTO;
 using SolessBackEndFix.Interfaces;
-using SolessBackEndFix.Models;
+using System;
 using System.Linq;
 
 namespace Examples.WebApi.Controllers
@@ -13,69 +14,52 @@ namespace Examples.WebApi.Controllers
         private readonly SmartSearchService _smartSearchService;
         private readonly IProductRepository _productRepository;
 
-        // Constructor con inyección de dependencias
         public SmartSearchController(SmartSearchService smartSearchService, IProductRepository productRepository)
         {
             _smartSearchService = smartSearchService;
             _productRepository = productRepository;
         }
 
-        // Acción para realizar la búsqueda de productos
         [HttpPost("Search")]
-        public IActionResult Search([FromQuery] string query = "",
-                                    [FromQuery] string sortField = "none",
-                                    [FromQuery] string sortOrder = "none",
-                                    [FromQuery] int page = 1,
-                                    [FromQuery] int limit = 10)
+        public IActionResult Search([FromBody] Search request)
         {
-            // Validar la paginación
-            if (page < 1 || limit < 1)
+            if (request.Page < 1 || request.Limit < 1)
             {
                 return BadRequest("La página y el límite deben ser mayores que 0.");
             }
 
             try
             {
-              
-                var products = string.IsNullOrWhiteSpace(query) ? _productRepository.GetProductsAsync().Result : _smartSearchService.Search(query);           
+                var products = string.IsNullOrWhiteSpace(request.Query)
+                    ? _productRepository.GetProductsAsync().Result
+                    : _smartSearchService.Search(request.Query);
 
                 if (products == null || !products.Any())
                 {
                     return NotFound("No products found.");
                 }
 
-                // Ordenar los productos si sortField es válido
-                if (sortField.ToLower() == "price")
+                if (request.SortField.ToLower() == "price")
                 {
-                    if (sortOrder.ToLower() == "asc")
-                    {
-                        products = products.OrderBy(p => p.Original_Price).ToList();
-                    }
-                    else if (sortOrder.ToLower() == "desc")
-                    {
-                        products = products.OrderByDescending(p => p.Original_Price).ToList();
-                    }
+                    products = request.SortOrder.ToLower() == "asc"
+                        ? products.OrderBy(p => p.Original_Price).ToList()
+                        : products.OrderByDescending(p => p.Original_Price).ToList();
                 }
-                else if (sortField.ToLower() == "name")
+                else if (request.SortField.ToLower() == "name")
                 {
-                    if (sortOrder.ToLower() == "asc")
-                    {
-                        products = products.OrderBy(p => p.Model).ToList();
-                    }
-                    else
-                    {
-                        products = products.OrderByDescending(p => p.Model).ToList();
-                    }
+                    products = request.SortOrder.ToLower() == "asc"
+                        ? products.OrderBy(p => p.Model).ToList()
+                        : products.OrderByDescending(p => p.Model).ToList();
                 }
 
                 var totalItems = products.Count();
-                var totalPages = (int)Math.Ceiling(totalItems / (double)limit);
+                var totalPages = (int)Math.Ceiling(totalItems / (double)request.Limit);
 
-                var paginatedProducts = products.Skip((page - 1) * limit).Take(limit).ToList();
+                var paginatedProducts = products.Skip((request.Page - 1) * request.Limit).Take(request.Limit).ToList();
 
                 var result = new
                 {
-                    currentPage = page,
+                    currentPage = request.Page,
                     totalPages = totalPages,
                     totalItems = totalItems,
                     items = paginatedProducts
--- SolessBackEndFix/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
+++ SolessBackEndFix/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83d4fa10b87a2cd0b0cf3fd3a5051c7846cafba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d8e7af3b5d659715746f04d8401e38218ec4e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
