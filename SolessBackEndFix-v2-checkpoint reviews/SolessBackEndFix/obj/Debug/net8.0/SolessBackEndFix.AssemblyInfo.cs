1c3e634bb17f5dcd134cdf47c41dd900995cdd5c
Christian Rodríguez Lara <christianrodriguezlara6@gmail.com>
2024-11-26 19:40:00 +0100

    Intento de solución de errores

--- SolessBackEndFix-v2-checkpoint reviews/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
+++ SolessBackEndFix-v2-checkpoint reviews/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7411ddc753a101a010145762b1560e543d8a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556f55636fd3732eac3f0f1571c0ed5ff571a1b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SolessBackEndFix-v3/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
+++ SolessBackEndFix-v3/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea350800290530b93d58696e30d5a303157398f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556f55636fd3732eac3f0f1571c0ed5ff571a1b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
