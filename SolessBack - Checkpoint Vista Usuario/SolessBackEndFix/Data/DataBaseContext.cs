8778ca555e048705263ee846c6674bc42063be7c
Josemi61 <jtorcan2903@g.educaand.es>
2024-12-03 19:58:43 +0100

    BackEnd punto de control vista admin

---
+++ SolessBack - Checkpoint Vista Usuario/SolessBackEndFix/Data/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolessBackend.Models;
+using SolessBackEndFix.Models;
+
+namespace SolessBackend.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        private const string DATABASE_PATH = "ecommerce.db";
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartProduct> CartProducts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            optionsBuilder.UseSqlite($"DataSource={baseDir}{DATABASE_PATH}");
+        }
+    }
+}
---
+++ SolessBack - Checkpoint Vista Usuario/SolessBackEndFix/Repositories/UserRepository.cs
@@ -0,0 +1,91 @@
+﻿using SolessBackend.Data;
+using SolessBackend.Interfaces;
+using SolessBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using SolessBackEndFix.Models;
+using SolessBackend.DTO;
+
+namespace SolessBackend.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataBaseContext _context;
+
+        public UserRepository(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<User>> GetUsersAsync()
+        {
+            return await _context.Users.OrderBy(u => u.Id).ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(long id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();  
+
+            var cart = new Cart
+            {
+                UserId = user.Id  
+            };
+
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();  
+        }
+
+        public async Task DeleteUserAsync(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateUserAsync(UserCreateDTO user)
+        {
+            var usuarioVariado = _context.Users.FirstOrDefault(p => p.Id == user.Id);
+
+            if (usuarioVariado == null)
+            {
+                throw new Exception("La variación del user no existe.");
+            }
+
+            usuarioVariado.Name = user.Name;
+            usuarioVariado.Email = user.Email;
+            usuarioVariado.Password = user.Password;
+            usuarioVariado.Address = user.Address;
+
+            _context.Users.Update(usuarioVariado);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAdminAsync(UserCreateDTO user)
+        {
+            var usuarioVariado = _context.Users.FirstOrDefault(p => p.Id == user.Id);
+
+            if (usuarioVariado == null)
+            {
+                throw new Exception("La variación del user no existe.");
+            }
+
+            usuarioVariado.Role = user.Role;
+
+            _context.Users.Update(usuarioVariado);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
