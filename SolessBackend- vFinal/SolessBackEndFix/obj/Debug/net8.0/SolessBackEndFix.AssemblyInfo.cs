81a7c60c2253ec0d6ef55fc661a8e2e964e36558
Christian Rodríguez Lara <christianrodriguezlara6@gmail.com>
2024-12-11 09:49:49 +0100

    Modificacion variable de entorno producción frontend

--- SolessBackend- vFinal/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
+++ SolessBackend- vFinal/SolessBackEndFix/obj/Debug/net8.0/SolessBackEndFix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e9e68e871688d4eaa22c5fcd45c355b42de89e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a136aeaafd5ca8e89038b0f7531e96f5da07941")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolessBackEndFix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
